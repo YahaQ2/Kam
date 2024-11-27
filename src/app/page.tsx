081522058fe0d436e20e524b9975285dd2d907a3
HengkerKucing <luqmanzzx@gmail.com>
2024-11-27 18:56:17 +0700

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
       <main className="flex-grow">
         <main className="container mx-auto px-4 py-8 md:py-16 text-center">
           <h2 className="text-3xl md:text-4xl font-bold mb-4 md:mb-6">
-            Ungkapan Perasaan Masyarakat Solinep
+            Menfess Masyarakat Polines
           </h2>
           <p className="text-base md:text-lg mb-8 md:mb-12">by pdf</p>
           <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4 mb-12 md:mb-16">
@@ -27,13 +27,13 @@ export default function HomePage() {
               asChild
               className="bg-gray-800 text-white px-6 md:px-8 py-2.5 md:py-3 rounded-full hover:bg-gray-900 transition-colors"
             >
-              <Link href="/message">Mulai Bercerita</Link>
+              <Link href="/message">Kirim Menfess</Link>
             </Button>
             <Button
               asChild
               className="border-2 border-gray-800 bg-white text-gray-800 px-6 md:px-8 py-2.5 md:py-3 rounded-full hover:bg-gray-100 transition-colors"
             >
-              <Link href="/search-message">Temukan Cerita</Link>
+              <Link href="/search-message">Cari Menfess</Link>
             </Button>
           </div>
           <div className="relative w-full max-w-7xl mx-auto overflow-hidden">
