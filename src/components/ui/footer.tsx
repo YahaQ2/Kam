3e438c7b679daaaa3ac320306a2be6b09386737f
FER PUTRA <feryputrafer@gmail.com>
2024-11-30 15:16:24 +0700

    update src/components/ui/footer.tsx

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,16 +6,16 @@ export function Footer() {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-            <p className="text-gray-600">&copy; 2024 Solifess. Terinspirasi dari website sendthesong.xyz</p>
+            <p className="text-gray-600">&copy; 2024 unandfess. Terinspirasi dari website sendthesong.xyz</p>
           </div>
           <div className="flex space-x-4">
             <a href="#" className="text-gray-600 hover:text-gray-800">
               <FaTwitter size={20} />
             </a>
-            <a href="https://www.instagram.com/thepdfway/" className="text-gray-600 hover:text-gray-800">
+            <a href="https://www.instagram.com/fer_.putra/" className="text-gray-600 hover:text-gray-800">
               <FaInstagram size={20} />
             </a>
-            <a href="https://github.com/thepdfway" className="text-gray-600 hover:text-gray-800">
+            <a href="https://github.com/ferput" className="text-gray-600 hover:text-gray-800">
               <FaGithub size={20} />
             </a>
           </div>
