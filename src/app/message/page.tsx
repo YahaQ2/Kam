2b1430d4c114914daed82325c8eb29b21cdd96d3
Hacoree <feryputrafer@gmail.com>
2024-12-03 01:00:54 +0700

    update src/app/message/page.tsx

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -43,7 +43,7 @@ export default function MulaiBerceritaPage() {
       setIsSearching(true);
       try {
         const response = await fetch(
-          `http://unand.vercel.app/v1/api/search-spotify-song?song=${encodeURIComponent(song)}`
+          `http://localhost:5000/v1/api/search-spotify-song?song=${encodeURIComponent(song)}`
         );
         const result = await response.json();
 
@@ -87,7 +87,7 @@ export default function MulaiBerceritaPage() {
     });
   
     try {
-      const response = await fetch("https://unand.vercel.app/v1/api/menfess-spotify", {
+      const response = await fetch("https://solifess.vercel.app/v1/api/menfess-spotify", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
