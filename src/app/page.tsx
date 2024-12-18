610b075c1e8fbd2ac928ad4648f8c2767f51b46e
YahaQ2 <vivopopupcera@gmail.com>
2024-12-18 22:37:35 +0700

    update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function HomePage() {
       setLoading(true);
       setError(null);
       try {
-        const response = await fetch(`https://unand.vercel.app/v1/api/menfess`);
+        const response = await fetch(`https://unand.vercel.app/v1/api/menfess/spotify-search`);
         if (!response.ok) {
           throw new Error("Failed to fetch messages.");
         }
