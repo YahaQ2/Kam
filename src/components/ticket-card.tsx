12a23b99bbea677a19ecafc2892202d4072c5f68
Hacoree <feryputrafer@gmail.com>
2024-12-18 08:56:37 +0700

    update src/components/ticket-card.tsx

--- src/components/ticket-card.tsx
+++ src/components/ticket-card.tsx
@@ -6,7 +6,7 @@ interface TickerCardProps {
 
 export const TickerCard: React.FC<TickerCardProps> = ({ messages }) => {
   return (
-    <div className="bg-white border border-gray-200 rounded-lg shadow-sm p-3 w-800 h-14 flex items-center overflow-hidden">
+    <div className="bg-white border border-gray-200 rounded-lg shadow-sm p-3 w-80 h-14 flex items-center overflow-hidden">
       <p className="w-full text-2xl text-gray-700 truncate font-['Reenie_Beanie']">{message}</p>
     </div>
   )
