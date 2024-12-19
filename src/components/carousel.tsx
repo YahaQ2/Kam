dc5bbd34a416ee09a22e3ebf26c341ca4d34766c
YahaQ2 <vivopopupcera@gmail.com>
2024-12-19 21:58:02 +0700

    update src/components/carousel.tsx

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -8,7 +8,7 @@ const messages = [
   { to: "al hme", from: "-", message: "kapan kita bisa main bareng?" },
   { to: "semua anak polines", from: "me", message: "guys jgn lupa jaga kebersihan klo udh makan minum, bungkus sampahnya WAJIB dibuang di tong sampah, meski di kantin jg harus begitu. dah gede ayo sadar kebersihan" },
   { to: "tesmank", from: "aldi", message: "jgn lupa me time" },
-  { to: "bang", from: "a", message: "Semangatt mbang, semoga segera wisuda yaaa. tutor dong jadi berkarisma" },
+  { to: "bang", from: "a", message: "Semangatt bang, semoga segera wisuda yaaa. tutor dong jadi berkarisma" },
   { to: "kelas a", from: "c", message: "kak dirimu sangat Masya Allah aselii" },
   { to: "mas-mas kandok", from: "ijo", message: "Kasih saran buat nambahin fitur baru" },
   { to: "mas-mas kandok", from: "ijo", message: "hai apa kabar" },
