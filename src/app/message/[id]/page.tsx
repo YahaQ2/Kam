d30a5040b2dedde180db8af5fe10b3f490378b70
YahaQ2 <vivopopupcera@gmail.com>
2024-12-20 14:46:09 +0700

    update src/app/message/[id]/page.tsx

--- src/app/message/[id]/page.tsx
+++ src/app/message/[id]/page.tsx
@@ -66,7 +66,7 @@ export default function MessagePage() {
     const fetchComments = async () => {
       try {
         const response = await fetch(
-          `https://unand.vercel.app/v1/api/menfess-spotify-search/?comment=${params.id}`
+          `https://unand.vercel.app/v1/api/menfess-spotify-search/${params.id}`
         );
         if (response.ok) {
           const data = await response.json();
