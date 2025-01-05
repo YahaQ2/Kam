ca633e25425dc057d544a54c3c1bdfd155b80369
YahaQ2 <vivopopupcera@gmail.com>
2025-01-06 00:10:31 +0700

    update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { WarningPopup } from "@/components/WarningPopup"; // sesuaikan dengan path file WarningPopup.js
+
 import { useEffect, useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Footer } from "@/components/ui/footer";
@@ -10,53 +10,7 @@ import dynamic from "next/dynamic";
 import { ArrowUpRight } from 'lucide-react';
 import { CarouselCard } from "@/components/carousel-card";
 import { motion } from "framer-motion";
-const WarningPopup = () => {
-  const [isVisible, setIsVisible] = useState(true);
 
-  const handleClose = () => {
-    setIsVisible(false);
-  };
-
-  if (!isVisible) return null;
-
-  return (
-    <div style={popupStyles}>
-      <div style={popupContentStyles}>
-        <p>Hai selamat ya!</p>
-        <button onClick={handleClose} style={closeButtonStyles}>Close</button>
-      </div>
-    </div>
-  );
-};
-
-const popupStyles = {
-  position: 'fixed',
-  top: '0',
-  left: '0',
-  width: '100%',
-  height: '100%',
-  backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-};
-
-const popupContentStyles = {
-  backgroundColor: 'white',
-  padding: '20px',
-  borderRadius: '5px',
-  textAlign: 'center',
-};
-
-const closeButtonStyles = {
-  marginTop: '10px',
-  padding: '5px 10px',
-  backgroundColor: '#f44336',
-  color: 'white',
-  border: 'none',
-  borderRadius: '3px',
-  cursor: 'pointer',
-};
 interface Menfess {
   id: number;
   sender: string;
