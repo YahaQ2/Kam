439f497fef30a2262417ff139eb2ae1ec6407915
YahaQ2 <vivopopupcera@gmail.com>
2025-02-04 04:58:03 +0700

    update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -127,7 +127,7 @@ export default function HomePage() {
         <div className="container mx-auto px-4 py-8 md:py-16 text-center">
           <h2 className="text-3xl md:text-4xl font-bold mb-4 md:mb-6">Menfess Masyarakat unand</h2>
           <Link
-            href="https://www.instagram.com/@unandfess.xyz"
+            href="https://forms.zohopublic.com/notnoting12gm1/form/Saran/formperma/8hcRs5pwX77B9AprPeIsvWElcwC1s3JJZlReOgJ3vdc"
             className="inline-flex items-center justify-center px-4 py-2 mb-8 text-sm md:text-base font-medium text-gray-600 hover:text-gray-800 transition-colors border border-gray-300 rounded-full hover:border-gray-400"
           >
             <span>saran/masukan/fitur baru</span>
