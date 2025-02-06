0d4cdbefc247e6c81ce53de7683e34f5c6217f0f
YahaQ2 <vivopopupcera@gmail.com>
2025-02-06 13:40:45 +0700

    update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,6 +83,18 @@ export default function HomePage() {
       setLoading(false);
     }
   };
+  async function getProfile(accessToken) {
+  let accessToken = localStorage.getItem('access_token');
+
+  const response = await fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    }
+  });
+
+  const data = await response.json();
+}
+
 
   useEffect(() => {
     fetchMessages();
