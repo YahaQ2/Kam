2f2bafff63024dfeac4f41597639fdb1e52b44fb
YahaQ2 <vivopopupcera@gmail.com>
2025-02-12 11:29:41 +0700

    update src/components/background-video.tsx

--- src/components/background-video.tsx
+++ src/components/background-video.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import ReactPlayer from 'react-player';
 
 export const BackgroundVideo = () => {
   const [isNight, setIsNight] = useState(false);
@@ -16,23 +15,23 @@ export const BackgroundVideo = () => {
   return (
     <div className="relative w-full h-full rounded-[40px] overflow-hidden border-2 border-white/20 m-4 shadow-2xl">
       {mounted && (
-        <ReactPlayer
-          url={isNight ? 'https://res.cloudinary.com/depbfbxtm/video/upload/v1739168678/Background_Video_Animasi_Awan_Bergerak_eovhes.mp4' : 'https://res.cloudinary.com/depbfbxtm/video/upload/v1739169325/Cartoon_Cloud_background___Free_motion_graphics_clouds_overlay___After_Effects_Clouds_animation_sk6kbr.mp4'}
-          playing
+        <video
+          key={isNight ? 'night-video' : 'day-video'}
+          autoPlay
           loop
           muted
-          width="100%"
-          height="100%"
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            minWidth: '100%',
-            minHeight: '100%',
-            objectFit: 'cover'
-          }}
-        />
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full object-cover"
+        >
+          <source
+            src={
+              isNight 
+                ? 'https://res.cloudinary.com/depbfbxtm/video/upload/v1739168678/Background_Video_Animasi_Awan_Bergerak_eovhes.mp4'
+                : 'https://res.cloudinary.com/depbfbxtm/video/upload/v1739169325/Cartoon_Cloud_background___Free_motion_graphics_clouds_overlay___After_Effects_Clouds_animation_sk6kbr.mp4'
+            }
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
       )}
     </div>
   );
