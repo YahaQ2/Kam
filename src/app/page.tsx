24706ada6cfee1e296b974d4c4733521117c5973
YahaQ2 <vivopopupcera@gmail.com>
2025-02-16 16:55:02 +0700

    update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -455,6 +455,7 @@ export default function HomePage() {
             </motion.div>
           </div>
         </section>
+  </div>
           <div className="relative w-full max-w-7xl mx-auto overflow-hidden mb-16">
             <DynamicCarousel />
           </div>
