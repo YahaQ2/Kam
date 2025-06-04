6113e789010d648c23679fbe7f3448f97738b369
YahaQ2 <vivopopupcera@gmail.com>
2025-06-04 14:39:15 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,7 @@ import AdSense from '@/components/AdSense'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <AdSense />
-      {}
-      {children}
-    </>
-  )
-}
+    
 // Fonts
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
